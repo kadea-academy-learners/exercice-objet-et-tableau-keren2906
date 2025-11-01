ac0a91befdc128f590ea568d7e473e30aa6af53d
keren2906 <kerenmatuli2906@gamil.com>
2025-11-01 09:51:28 +0100

    feat: implement authentication system with signUp and login, handle errors and user states

--- 05-authentification-system.js
+++ 05-authentification-system.js
@@ -18,14 +18,64 @@
 //    - Si l'utilisateur est bloqué (`estBloque` à true), retourne un message d'erreur spécifique.
 //    - Sinon, met à jour `estConnecte` à true pour cet utilisateur et retourne l'objet utilisateur connecté.
 
+
 const baseDeDonnees = [];
 
 function signUp(nom, email, password, confirmPassword) {
-	
+     const utilisateurExistant = baseDeDonnees.find(user => user.email === email);
+    if (utilisateurExistant) {
+        return "Erreur: cet email existe déjà";
+    }
+
+    // Vérification du mot de passe
+    if (password !== confirmPassword) {
+        return "Erreur: les mots de passe ne correspondent pas";
+    }
+    const nouvelUtilisateur = {
+        id: baseDeDonnees.length + 1,
+        nom: nom,
+        email: email,
+        password: password,
+        estConnecte: false,
+        estBloque: false,
+    };
+
+    // Ajouter dans la base de données
+    baseDeDonnees.push(nouvelUtilisateur);
+
+    return nouvelUtilisateur;
 }
 
-function login() {
-	
+function login(email, password) {
+     const utilisateur = baseDeDonnees.find(user => user.email === email);
+
+    // Vérification de l'utilisateur existant
+    if (!utilisateur) {
+        return "Erreur: utilisateur non trouvé";
+    }
+
+    // Vérification du mot de passe
+    if (utilisateur.password !== password) {
+        return "Erreur: mot de passe incorrect";
+    }
+
+    // Vérifier si le compte est bloqué
+    if (utilisateur.estBloque) {
+        return "Erreur: utilisateur bloqué";
+    }
+
+    // Connexion réussie
+    utilisateur.estConnecte = true;
+    return utilisateur;
 }
+
+
+console.log(signUp("Alice", "alice@example.com", "1234", "1234")); // inscription
+console.log(signUp("Alice", "alice@example.com", "1234", "1234")); // email déjà utilisé
+console.log(login("alice@example.com", "1234")); // connexion réussie
+console.log(login("bob@example.com", "1234")); // utilisateur introuvable
+console.log(login("alice@example.com", "0000")); // mot de passe incorrect
 
 module.exports = { baseDeDonnees, signUp, login };
+
+
