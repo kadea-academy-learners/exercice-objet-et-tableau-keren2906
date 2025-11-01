30ca0d786337936a530b0cfa91b27da35f0d8f05
keren2906 <kerenmatuli2906@gamil.com>
2025-11-01 09:17:45 +0100

    feat: add whoIsAdmin function to return the names of admin users

--- 03-user-management.js
+++ 03-user-management.js
@@ -5,12 +5,25 @@
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin() {
-	
+
+function whoIsAdmin(utilisateurs) {
+
+    const admins = utilisateurs.filter(user => user.estAdmin === true);
+
+    return admins.map(user => user.nom);
 }
 
+const utilisateurs =[
+    { nom: "Alice", age: 25, estAdmin: true },
+    { nom: "Bob", age: 30, estAdmin: false },
+    { nom: "Charlie", age: 28, estAdmin: true },
+];
+
+console.log(whoIsAdmin(utilisateurs));
+
+// Export pour les tests si nécessaire
 module.exports = {
-	whoIsAdmin,
+    whoIsAdmin,
 };
 
 
