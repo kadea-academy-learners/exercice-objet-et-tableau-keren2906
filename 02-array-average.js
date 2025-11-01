c91079e00928f384c5ab243634bca4e92ef51b01
keren2906 <kerenmatuli2906@gamil.com>
2025-11-01 08:53:21 +0100

    feat: add averageNote function to calculate the average and display Passed or Failed

--- 02-array-average.js
+++ 02-array-average.js
@@ -3,9 +3,21 @@
 // - Calcule la moyenne des notes
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
 
+// creation du tableau
+const notes = [18, 9, 15, 20, 4];
 function averageNote(notes) {
-	
+	const total = notes.reduce((sum,note)=> sum + note,0);
+	// calcul de la moyenne
+	const moyenne = total /notes.length;
+	// les conditions
+	if(moyenne >= 10){
+		return "Réussi";
+	} else {
+		return "Échoué";
+	}
 }
+// affichage dans la console
+console.log(averageNote (notes));
 
 module.exports = {
 	averageNote,
